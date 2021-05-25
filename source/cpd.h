b7348f6ad34e839a443336bbf813fddfecfe4de3
Nick <nicholasmcdonald40@gmail.com>
2021-05-26 01:15:51 +0200

    Cleaned the CPD file and removed the executable

--- source/cpd.h
+++ source/cpd.h
@@ -1,6 +1,7 @@
 /*
 
 Coherent Point Drift Algorithm!
+Author: Nicholas McDonald
 
 */
 
@@ -11,19 +12,163 @@ Coherent Point Drift Algorithm!
 #define PI 3.14159265f
 
 namespace cpd {
+
 using namespace Eigen;
 using namespace std;
 using namespace glm;
 
+//All the properties needed for the algorithm below!
+
+float D = 0.0f;   //Dimensionality of Pointsets
+float N = 0.0f;   //Size of Sampled Pointset
+float M = 0.0f;   //Size of Centroid Pointset
+
+float s;          //Scale
+Matrix3f R;       //Rotation
+Vector3f t;       //Translation
+float var;        //Variance
+
+float w = 0.0f;   //Noise Estimate (Free Parameter)
+MatrixXf P;       //Posterior Probability Matrix (Assignments)
+
+//Options
+bool enforcescale = true; //Force Scale to be s = 1
+
 /*
 ================================================================================
-                    Pointset Representation Translation
+              Initialization, Estimation and Maximization Steps
 ================================================================================
 */
 
-float D = 0.0f;  //Dimensionality of Pointsets
-float N = 0.0f;  //Size of Sampled Pointset
-float M = 0.0f;  //Size of Centroid Pointset
+void initialize(MatrixXf& X, MatrixXf& Y){
+
+  s = 1.0f;                     //Initialize Scale to 1
+  R = Matrix3f::Identity();     //Initialize Rotation Matrix to Identity
+  t = Vector3f::Zero();         //Initialize Translation Vector to Zero
+
+  N = X.rows();                 //Get the Number of Points in "Sampled Set"
+  M = Y.rows();                 //Get the Number of Points in "Centroid Set"
+
+  var = 0.0f;                   //Compute the Worst-Case Variance
+  for(size_t n = 0; n < N; n++)
+  for(size_t m = 0; m < M; m++){
+    Vector3f d = (X.block<1,3>(n,0)-Y.block<1,3>(m,0)); //Distance Between Two Points x_n, y_m
+    var += d.dot(d)/(float)(D*N*M);                     //Normalize and Add
+  }
+
+  P = MatrixXf::Zero(M,N);      //Allocate Memory for Probability Matrix
+
+}
+
+void estimate(MatrixXf& X, MatrixXf& Y){
+
+  //Individual Assignment Probability for x_n, y_m!
+  const function<float(Vector3f, Vector3f)> Pmn = [](Vector3f x, Vector3f y){
+    Vector3f d = (x-s*R*y-t);       //Rigid Transform
+    return exp(-0.5f/var*d.dot(d)); //Gaussian Probability
+  };
+
+  //Constant Normalization Bias
+  const float bias = pow(2.0f*PI*var, D/2.0f)*w/(1.0f-w)*M/N;
+
+  for(size_t n = 0; n < N; n++)     //Full Probability Matrix
+  for(size_t m = 0; m < M; m++)
+    P(m,n) = Pmn(X.block<1,3>(n,0), Y.block<1,3>(m,0));
+
+  for(size_t n = 0; n < N; n++){    //Normalize
+    float Z = bias;                 //Add Bias
+    for(size_t m = 0; m < M; m++)   //Accumulate
+      Z += P(m,n);
+    for(size_t m = 0; m < M; m++)   //Divide
+      P(m,n) /= Z;
+  }
+
+}
+
+void maximize(MatrixXf& X, MatrixXf& Y){
+
+  float Np = P.sum();                                     //Normalization Constant
+  VectorXf P1 = P*MatrixXf::Ones(N,1);                    //Sum over all n
+  VectorXf PT1 = P.transpose()*MatrixXf::Ones(M,1);       //Sum over all m
+  Vector3f uX = X.transpose()*PT1/Np;                     //Average Position, X-Set
+  Vector3f uY = Y.transpose()*P1/Np;                      //Average Position, Y-Set
+
+  MatrixXf XC = X - MatrixXf::Ones(N,1)*uX.transpose();   //Centered X-Set
+  MatrixXf YC = Y - MatrixXf::Ones(M,1)*uY.transpose();   //Centered Y-Set
+
+  MatrixXf A = XC.transpose()*P.transpose()*YC;           //Singular Value Decomp. Matrix
+
+  JacobiSVD<MatrixXf> svd(A, ComputeFullU|ComputeFullV);  //Compute the SVD of A
+  MatrixXf U = svd.matrixU();                             //Get the SVD Matrix U
+  MatrixXf V = svd.matrixV();                             //Get the SVD Matrix V
+
+  MatrixXf C = MatrixXf::Zero(D, D);                      //Construct the SVD-Derived Matrix C
+  for(int i = 0; i < D-1; i++)
+    C(i, i) = 1;
+  C(D-1, D-1) = (U*V.transpose()).determinant();
+
+  //Compute the Rigid Transformation Parameters
+  R = U*C*V.transpose();
+  s = (A.transpose()*R).trace()/(YC.transpose()*P1.asDiagonal()*YC).trace();
+  t = uX - s*R*uY;
+
+  //Recompute Standard Deviation
+  var = 1.0f/(Np*D)*((XC.transpose()*PT1.asDiagonal()*XC).trace() - s*(A.transpose()*R).trace());
+
+  if(enforcescale) s = 1.0f;  //Enforce Constant Scale
+
+}
+
+/*
+================================================================================
+                          Iteration and Utilization
+================================================================================
+*/
+
+void output(){
+  cout<<"Rigid Transform (Var = "<<var<<"): "<<endl;
+  cout<<"R "<<R<<endl;
+  cout<<"t "<<t<<endl;
+  cout<<"s "<<s<<endl;
+}
+
+
+//Single Iteration Step
+float oldvar = 0.0f;
+bool itersolve(MatrixXf& X, MatrixXf& Y, int& N, const float tol = 0.01f){
+
+  if(N-- <= 0 || abs(oldvar - var) <= tol) return false;
+
+  oldvar = var;
+  cout<<"Iteration: "<<N<<endl;
+  cout<<"Estimating..."<<endl;
+  cpd::estimate(X, Y);
+  cout<<"Maximizing..."<<endl;
+  cpd::maximize(X, Y);
+  cpd::output();
+  return true;
+
+}
+
+void solve(MatrixXf& X, MatrixXf& Y, const int maxN, const float tol = 0.01f){
+  int niter = maxN;
+  while(itersolve(X, Y, niter, tol));
+}
+
+
+mat4 rigid(){      //Extract a glm-based 4x4 transformation matrix
+  mat4 transform = {s*R(0,0), s*R(0,1), s*R(0,2), t(0),
+                    s*R(1,0), s*R(1,1), s*R(1,2), t(1),
+                    s*R(2,0), s*R(2,1), s*R(2,2), t(2),
+                    0,        0,        0,        1.0f};
+  return transform;
+}
+
+/*
+================================================================================
+                Pointset Representation Translation (Helpers)
+================================================================================
+*/
 
 MatrixXf makeset(vector<vec2>& pointset){
   MatrixXf S(pointset.size(), 2);
@@ -44,174 +189,6 @@ MatrixXf makeset(vector<vec3>& pointset){
   }
   D = 3;
   return S;
-}
-
-/*
-================================================================================
-                      Rigid Transformation Formulation
-================================================================================
-*/
-
-float s;        //Scale
-Matrix3f R;     //Rotation
-Vector3f t;     //Translation
-float var;      //Variance
-
-float w = 0.0f; //Noise Estimate (Free Parameter)
-MatrixXf P;     //Posterior Probability Matrix (Assignments)
-
-void initialize(MatrixXf& X, MatrixXf& Y){
-
-  //Rigid Transformation Parameters
-  s = 1.0f;
-  R = Matrix3f::Identity();
-  t = Vector3f::Zero();
-
-  //Get Size Parameters
-  N = X.rows();
-  M = Y.rows();
-
-  //Variance (Worst-Case)
-  var = 0.0f;
-  for(size_t n = 0; n < N; n++)
-    for(size_t m = 0; m < M; m++){
-      Vector3f d = (X.block<1,3>(n,0)-Y.block<1,3>(m,0));
-      var += d.dot(d)/(float)(D*N*M);
-    }
-
-  //Allocate Memory for the Probability Matrix
-  P = MatrixXf::Zero(M,N);
-
-}
-
-void estimate(MatrixXf& X, MatrixXf& Y){
-
-  std::cout<<"Estimating..."<<std::endl;
-
-  //Individual Assignment Probability!
-  const function<float(Vector3f, Vector3f)> Pmn = [](Vector3f x, Vector3f y){
-    Vector3f d = (x-s*R*y-t);       //Rigid Transform
-    return exp(-0.5f/var*d.dot(d)); //Gaussian Probability
-  };
-
-  //Constant Normalization Bias
-  const float bias = pow(2.0f*PI*var, D/2.0f)*w/(1.0f-w)*M/N;
-
-  //Compute the Probability Matrix!
-  for(size_t n = 0; n < N; n++)
-    for(size_t m = 0; m < M; m++)
-      P(m,n) = Pmn(X.block<1,3>(n,0), Y.block<1,3>(m,0));
-
-  //Normalize
-  for(size_t n = 0; n < N; n++){
-
-    //Accumulate
-    float Z = bias;
-    for(size_t m = 0; m < M; m++)
-      Z += P(m,n);
-
-    //Divide for all m belonging to n
-    for(size_t m = 0; m < M; m++)
-      P(m,n) /= Z;
-
-  }
-
-}
-
-void maximize(MatrixXf& X, MatrixXf& Y){
-
-  std::cout<<"Maximizing..."<<std::endl;
-
-  //Compute Helper Quantities
-  float Np = P.sum(); //Normalization Constant
-  VectorXf P1 = P*MatrixXf::Ones(N,1);
-  VectorXf PT1 = P.transpose()*MatrixXf::Ones(M,1);
-  Vector3f uX = X.transpose()*PT1/Np; //Average Position X
-  Vector3f uY = Y.transpose()*P1/Np; //Average Position X
-
-  //Centralize X, Y
-  MatrixXf XC = X - MatrixXf::Ones(N,1)*uX.transpose();
-  MatrixXf YC = Y - MatrixXf::Ones(M,1)*uY.transpose();
-
-  //Singular Value Decomposition
-  MatrixXf A = XC.transpose()*P.transpose()*YC;
-
-  JacobiSVD<MatrixXf> svd(A, ComputeFullU | ComputeFullV);
-  MatrixXf U = svd.matrixU();
-  MatrixXf V = svd.matrixV();
-
-  MatrixXf C = MatrixXf::Zero(U.cols(), V.rows());
-  for(int i = 0; i < C.cols() && i < C.rows(); i++)
-    C(i, i) = 1;
-
-  if(C.cols() < C.cols()) C(C.cols()-1, C.cols()-1) = (U*V.transpose()).determinant();
-  else C(C.rows()-1, C.rows()-1) = (U*V.transpose()).determinant();
-
-  //Compute the Derived Quantities
-  R = U*C*V.transpose();
-  s = (A.transpose()*R).trace()/(YC.transpose()*P1.asDiagonal()*YC).trace();
-  t = uX - s*R*uY;
-
-  //Recompute the Standard Deviation
-  var = 1.0f/(Np*D)*((XC.transpose()*PT1.asDiagonal()*XC).trace() - s*(A.transpose()*R).trace());
-
-  //Reset Scale?
-  s = 1.0f;
-
-}
-
-void output(){
-
-  std::cout<<"Rigid Transform (Err = "<<var<<"): "<<std::endl;
-  std::cout<<"R "<<R<<std::endl;
-  std::cout<<"t "<<t<<std::endl;
-  std::cout<<"s "<<s<<std::endl;
-
-}
-
-float oldvar = 0.0f;
-void solve(MatrixXf& X, MatrixXf& Y, const int maxN, const float tol = 0.01f){
-
-  for(int i = 0; i < maxN && abs(oldvar - var) > tol; i++){
-    oldvar = var;
-    std::cout<<"Iteration: "<<i<<std::endl;
-    cpd::estimate(X, Y);
-    cpd::maximize(X, Y);
-    cpd::output();
-  }
-
-}
-
-//Single Step!
-void itersolve(MatrixXf& X, MatrixXf& Y, int& N, const float tol = 0.01f){
-
-  if(N-- <= 0 || abs(oldvar - var) <= tol) return;
-
-  oldvar = var;
-  std::cout<<"Iteration: "<<N<<std::endl;
-  cpd::estimate(X, Y);
-  cpd::maximize(X, Y);
-  cpd::output();
-
-}
-
-mat4 rigid(){
-
-  mat4 transform = mat4(1);
-  transform[0][0] = s*R(0,0);
-  transform[0][1] = s*R(0,1);
-  transform[0][2] = s*R(0,2);
-  transform[1][0] = s*R(1,0);
-  transform[1][1] = s*R(1,1);
-  transform[1][2] = s*R(1,2);
-  transform[2][0] = s*R(2,0);
-  transform[2][1] = s*R(2,1);
-  transform[2][2] = s*R(2,2);
-  transform[0][3] = t(0);
-  transform[1][3] = t(1);
-  transform[2][3] = t(2);
-  return transform;
-
 }
 
 }
