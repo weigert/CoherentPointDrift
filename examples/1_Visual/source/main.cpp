7eb4d1ca7b3b4ee1464dfe1acec4eebce0575cd5
Nick <nicholasmcdonald40@gmail.com>
2021-05-26 12:34:33 +0200

    Fixed which pointcloud is mapping to which

--- examples/1_Visual/source/main.cpp
+++ examples/1_Visual/source/main.cpp
@@ -22,21 +22,13 @@ int main( int argc, char* args[] ) {
 	std::vector<glm::vec3> pointsA = shape::cube(1000, glm::vec3(0.25, 0.5, 1.0f));
 	std::vector<glm::vec3> pointsB = shape::cube(1000, glm::vec3(0.25, 0.5, 1.0f));
 
-	glm::mat4 rot = shape::randrot();
+	glm::mat4 rtrans = shape::randomtransform();
 
 	//Apply Transformation and Noise
 	for(auto& point: pointsB)
-		point = glm::vec3(rot*glm::vec4(point, 1.0f));
+		point = glm::vec3(rtrans*glm::vec4(point, 1.0f));
 	shape::noise(pointsA, 2.0f); //Add Gaussian Noise
 	shape::noise(pointsB, 2.0f); //Add Gaussian Noise
-
-
-
-
-
-
-
-
 
 	//Compute the Theoretical Rigid Transformation
 	Eigen::MatrixXf X = cpd::makeset(pointsA);
@@ -61,10 +53,10 @@ int main( int argc, char* args[] ) {
 
 				glm::mat4 transform = cpd::rigid();
 
-				for(auto& point: pointsA)
+				for(auto& point: pointsB)
 					point = glm::vec3(transform*glm::vec4(point, 1.0f));
 
-				X = cpd::makeset(pointsA);
+				Y = cpd::makeset(pointsB);
 				cpd::initialize(X, Y);
 
 			}
@@ -78,9 +70,6 @@ int main( int argc, char* args[] ) {
 		cam::handler();
 
 	};
-
-
-	//std::cout<<"Done!"<<std::endl;
 
 	//Construct the Models for the PointCloud!
 	std::vector<glm::mat4> models;
@@ -135,7 +124,7 @@ int main( int argc, char* args[] ) {
 			glm::mat4 model = glm::mat4(1.0);
 			model = glm::scale(glm::mat4(1.0), glm::vec3(0.5))*model;
 			model = glm::rotate(glm::mat4(1.0), glm::radians(90.0f-cam::rot), glm::vec3(0,1,0))*model;
-			model = glm::translate(glm::mat4(1.0), glm::vec3(transform*glm::vec4(point, 1.0f)))*model;
+			model = glm::translate(glm::mat4(1.0), point)*model;
 			models.push_back(model);
 		}
 
@@ -143,7 +132,7 @@ int main( int argc, char* args[] ) {
 			glm::mat4 model = glm::mat4(1.0);
 			model = glm::scale(glm::mat4(1.0), glm::vec3(0.5))*model;
 			model = glm::rotate(glm::mat4(1.0), glm::radians(90.0f-cam::rot), glm::vec3(0,1,0))*model;
-			model = glm::translate(glm::mat4(1.0), point)*model;
+			model = glm::translate(glm::mat4(1.0), glm::vec3(transform*glm::vec4(point, 1.0f)))*model;
 			models.push_back(model);
 		}
 
